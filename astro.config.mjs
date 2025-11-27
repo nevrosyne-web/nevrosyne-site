ee64369c700a20d11b9c0fce36bb566266deb6b8
fabien <fabien.petitpas@univ-amu.fr>
2025-11-27 08:57:22 +0100

    simplification fichier astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@tailwindcss/vite';   // <-- CE NOM-LÀ
-
-// https://astro.build/config
-// export default defineConfig({
-//   site: 'https://nevrosyne5ch.netlify.app', // ← remplace par ton URL
-//   integrations: [sitemap()],
-//   vite: {
-//     plugins: [tailwindcss()]
-//   }
-// });
+import tailwind from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://nevrosyne-web.github.io/nevrosyne-site/',
   base: '/nevrosyne-site/',
   integrations: [sitemap()],
   vite: {
-    plugins: [tailwind()],                  // <-- ET CE NOM-LÀ
+    plugins: [tailwind()],
   },
 });
